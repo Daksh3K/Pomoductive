9b53d84f21519ae46778ee4d9a98a66dbfe6a034
Dakshesh Gupta <awesomedakshesh@gmail.com>
2021-09-24 02:24:29 -0400

    Added Components

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import TodoItem from './components/TodoItem'
+import todosData from "./todosData"; // to-do list data
 
 
 function App(){
+    let todoItems = todosData.map(todoItem => <TodoItem key={todoItem.id} todoItem={todoItem}/>)
+
     return(
     <div className="todo-list">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todoItems}
     </div>
     )
 }
--- react-intro/src/components/TodoItem.js
+++ react-intro/src/components/TodoItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function TodoItem(){
+function TodoItem(props){
     return (
         <div className="todo-items">
-            <input type="checkbox" />
-            <p>Placeholder Text</p>
+            <input type="checkbox" checked={props.todoItem.completed}/>
+            <p>{props.todoItem.text}</p>
         </div>
     )
 }
+
+// BUG: todo-item 4 checkbox is misaligned due to checking it
 
 export default TodoItem;
