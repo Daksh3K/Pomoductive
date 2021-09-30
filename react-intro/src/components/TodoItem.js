31967f0b29373ae2c5d34a657326ccddbc72b8c4
Dakshesh Gupta <awesomedakshesh@gmail.com>
2021-09-30 09:33:48 -0400

    class components

--- react-intro/src/components/TodoItem.js
+++ react-intro/src/components/TodoItem.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function TodoItem(props){
-    return (
-        <div className="todo-items">
-            <input type="checkbox" checked={props.todoItem.completed}/>
-            <p>{props.todoItem.text}</p>
-        </div>
-    )
+class TodoItem extends React.Component() {
+    render() {
+        return(
+            <div className="todo-items">
+                <input type="checkbox" checked={this.props.todoItem.completed}/>
+                <p>{this.props.todoItem.text}</p>
+            </div>
+        )
+    }
 }
+
+
+// function TodoItem(props){
+//     return (
+//         <div className="todo-items">
+//             <input type="checkbox" checked={props.todoItem.completed}/>
+//             <p>{props.todoItem.text}</p>
+//         </div>
+//     )
+// }
 
 // BUG: todo-item 4 checkbox is misaligned due to checking it
 
