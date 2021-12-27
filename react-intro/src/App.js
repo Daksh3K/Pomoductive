b8927b968f8e0b06451b2a7d75f82921825c3205
Dakshesh <daksheshgupta03@gmail.com>
2021-12-27 09:00:54 -0500

    added delete, checking functionality

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,102 +1,124 @@
 import React from "react";
-import TodoItem from './components/TodoItem'
+import TodoItem from "./components/TodoItem";
 import todosData from "./todosData"; // to-do list data
 
-var todos = todosData
+var todos = todosData;
 
-class App extends React.Component{
-    constructor() {
-        super()
-        this.state = {todos: todos, text: ""}
-        this.handleItemCheck = this.handleItemCheck.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+class App extends React.Component {
+  /**
+   * Native react constructor method
+   */
+  constructor() {
+    super();
+    this.state = { todos: todos, text: "" };
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleItemCheck = (childState) => {
-        var counter = 0;
-        this.setState((prevState) => {
-            for (var item of prevState.todos) {
-                if (item.id === childState.id) {
-                    if (childState.checked === true) {
-                        prevState.todos.splice(counter, 1)
-                    }
-                }
-                counter ++
+  /**
+   * Function is called whenever a todo-item is checked
+   * @param {object} childState
+   */
+  handleItemDelete = (childID) => {
+    this.setState((prevState) => {
+        for (var [index, item] of prevState.todos.entries()) {
+            if (item.id === parseInt(childID)) {
+                console.log("found")
+                prevState.todos.splice(index, 1);
             }
-
-            return {todos: prevState.todos}
-        })
-    }
-
-    generateUniqueID = () => {
-        do {
-            var isFound = false;
-            var id = Math.floor(Math.random() * 10)
-            for (var item of todos) {
-                if (item.id === id) {
-                    isFound = true;
-                }
-            }
-        } while (isFound)
-        return id;
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        if (this.state.text.length === 0) {
-            return;
         }
-        
-        const newItem = {
-            // TODO: add a check to make sure the id generated is unique.
-            id: this.generateUniqueID(),
-            text: this.state.text,
-            completed: false
+        return { todos: prevState.todos };
+    })
+  };
+
+  /**
+   * Helper function to generate unique IDs
+   * @returns a unique ID
+   */
+  generateUniqueID = () => {
+    do {
+      var isFound = false;
+      var id = Math.floor(Math.random() * 100);
+      for (var item of todos) {
+        if (item.id === id) {
+          isFound = true;
         }
+      }
+    } while (isFound);
+    return id;
+  };
 
-        this.setState((prevState) => {
-            var prevTodos = prevState.todos
-            prevTodos.push(newItem)
-            return ({todos: prevTodos, text: ""}) 
-        }, () => console.log(typeof this.state.todos))
+  /**
+   * Function is called whenever user enters/clicks add button
+   * It adds a new todo item to the array and re-renders it
+   * @param {*} e
+   * @returns
+   */
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
     }
 
+    const newItem = {
+      // TODO: add a check to make sure the id generated is unique.
+      id: this.generateUniqueID(),
+      text: this.state.text,
+      completed: false,
+    };
 
-    handleChange = (e) => {
-        this.setState({text: e.target.value})
-    }
+    this.setState(
+      (prevState) => {
+        var prevTodos = prevState.todos;
+        prevTodos.push(newItem);
+        return { todos: prevTodos, text: "" };
+      },
+      () => console.log('new item created')
+    );
+  };
 
+  /**
+   * Function is called whenever the user interactions with input field
+   * @param {*} e
+   */
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
 
-    render() {
-        console.log(this.state.todos)
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} items={item} changeFunction={this.handleItemCheck} />)
-        return(
-            <div className="list-container">
+  /**
+   * Native react render method
+   * @returns
+   */
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem
+        key={item.id}
+        items={item}
+        changeFunction={this.handleItemDelete}
+      />
+    ));
+    return (
+      <main>
+        <div className="todo-container elevation-1">
+          <h1 className="todo-heading"> To-Do List </h1>
+          <hr className="todo-heading-line" />
 
-                <h1 className="heading"> To-Do List </h1>
-                <hr className="heading-line"/>
+          {todoItems}
 
-
-                {todoItems}
-
-                <div className="input-container">
-                    <form onSubmit={this.handleSubmit} className="input-container-flex">
-                        <input
-                            className="list-input"
-                            id="todo-input"
-                            onChange={this.handleChange}
-                            value={this.state.text}
-                            className="list-input"
-                        />
-                        <button className="list-input-button">+</button>
-                    
-                    </form>
-                </div>
-            </div>
-        )
-
-    }
-
+          <div className="todo-input-container">
+            <form onSubmit={this.handleSubmit} className="todo-container-flex">
+              <input
+                className="todo-input"
+                id="todo-input"
+                onChange={this.handleChange}
+                value={this.state.text}
+              />
+              <button className="todo-input-button">+</button>
+            </form>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
-
 export default App;
--- react-intro/src/components/TodoItem.js
+++ react-intro/src/components/TodoItem.js
@@ -1,44 +1,72 @@
 import React from "react";
 import "./TodoItem.css";
 
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
 
-class TodoItem extends React.Component{
+    this.state = {
+      checked: this.props.items.completed,
+      id: this.props.items.id,
+    };
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
+  handleCheck() {
+    this.setState((prevState) => {
+      return { checked: !prevState.checked };
+    });
+  }
 
-        this.state = {
-            checked: this.props.items.completed,
-            id: this.props.items.id
-        }
-        this.handleCheck = this.handleCheck.bind(this);
+  handleDelete = (e) => {
+    this.props.changeFunction(e.currentTarget.id);
+  };
+
+  render() {
+    let textDecor;
+    if (this.state.checked) {
+      textDecor = "line-through";
+    } else {
+      textDecor = null;
     }
 
+    return (
+      <div className="todo-item-container">
+        <label
+          for={this.state.id}
+          className="todo-item-text"
+          style={{ textDecoration: textDecor }}
+        >
+          {this.props.items.text}
+        </label>
 
-    handleCheck() {
-        this.setState(prevState => { // NOTE: this.setState() is asynchornous
-            return {checked: !prevState.checked}
-        }, () => this.props.changeFunction(this.state)) // wtf is this, 
-        // why do i need an arrow function for a callback, is this a js moment
-    }
-
-
-    render() {
-        let textDecor;
-        if (this.state.checked) {
-            textDecor = "line-through"
-        } else {
-            textDecor = null;
-        }
-
-        return(
-            <div className="list-item">
-                <p className="input-text" style={{textDecoration:textDecor}}>{this.props.items.text}</p>
-                <input className="input-checkbox" type="checkbox" checked={this.state.checked} onChange={this.handleCheck}/>
-            </div>
-        )
-    }
+        <div className="todo-item-buttons">
+          <button onClick={this.handleDelete} id={this.state.id}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+          <input
+            id={this.state.id}
+            className="todo-item-checkbox"
+            type="checkbox"
+            checked={this.state.checked}
+            onChange={this.handleCheck}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default TodoItem;
